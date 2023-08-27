85c777b54c193446f24bc508303c41fea58f16d0
Iurii Kleimenov <yurakleimenovv@gmail.com>
2023-08-27 22:48:27 +0300

    Created ViewControler and CoinManager

---
+++ Coin Corner/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  Coin Corner
+//
+//  Created by Юрий Клеймёнов on 27/08/2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+}
