193723531957aa4e424b333e617b8b777ab52286
Iurii Kleimenov <yurakleimenovv@gmail.com>
2023-08-27 22:23:45 +0300

    Initial Commit

---
+++ Coin Corner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin Corner
+//
+//  Created by Юрий Клеймёнов on 27/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
